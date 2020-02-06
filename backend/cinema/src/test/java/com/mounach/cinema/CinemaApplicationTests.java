4759d3d31891c16c7ecfb0607e1de61aafec45cd
Mouad MOUNACH <mounach@gmail.com>
2020-02-06 23:25:25 +0100

    init backend && frontend && gitignore

---
+++ backend/cinema/src/test/java/com/mounach/cinema/CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mounach.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
